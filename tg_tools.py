5dfb180e2ed1f133d4d556edec333959402a019c
Kirmen <kirmen.vlada@gmail.com>
2024-01-07 13:26:44 +0200

    correction...

--- tg_tools.py
+++ tg_tools.py
@@ -1,6 +1,10 @@
-import bot_data
+import os
+from typing import List
 
 from telegram import Bot
+
+from photo_handlers import download_and_convert_photos
+import bot_data
 
 
 def send_message(message: str):
@@ -10,13 +14,16 @@ def send_message(message: str):
     bot.send_message(chat_id=chat_id, text=message)
 
 
-def send_message_with_photos(message: str, photos: str):
+def send_message_with_photos(message: str, photos: List[str]):
     token = bot_data.TOKEN
     chat_id = bot_data.CHAT_ID
     bot = Bot(token=token)
 
     bot.send_message(chat_id=chat_id, text=message)
 
-    # for photo in photos:
-    #     with open(photo, 'rb') as f:
-    #         bot.send_photo(chat_id=CHAT_ID, photo=f)
+    converted_photos = download_and_convert_photos(photos)
+    for photo in converted_photos:
+        with open(photo, 'rb') as f:
+            bot.send_photo(chat_id=chat_id, photo=f)
+
+        os.remove(photo)
