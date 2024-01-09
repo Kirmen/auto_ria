aa69fed78b04a38eb87f44ecc9c46e9197d50ef8
Kirmen <kirmen.vlada@gmail.com>
2024-01-09 23:22:11 +0200

    fix tg-bot

--- tg_tools.py
+++ tg_tools.py
@@ -1,29 +1,43 @@
 import os
 from typing import List
+import configparser
 
-from telegram import Bot
+import requests
 
 from photo_handlers import download_and_convert_photos
-import bot_data
+
+TELEGRAM_API_URL = "https://api.telegram.org/bot"
+
+config = configparser.ConfigParser()
+config.read('bot_data.config')
+
+TOKEN = config.get('DEFAULT', 'TOKEN')
+CHAT_ID = config.get('DEFAULT', 'CHAT_ID')
 
 
 def send_message(message: str):
-    token = bot_data.TOKEN
-    chat_id = bot_data.CHAT_ID
-    bot = Bot(token=token)
-    bot.send_message(chat_id=chat_id, text=message)
+    token = TOKEN
+    chat_id = CHAT_ID
+    url = f"{TELEGRAM_API_URL}{token}/sendMessage"
+    params = {"chat_id": chat_id, "text": message}
+    response = requests.get(url, params=params)
+    return response.json()
 
 
 def send_message_with_photos(message: str, photos: List[str]):
-    token = bot_data.TOKEN
-    chat_id = bot_data.CHAT_ID
-    bot = Bot(token=token)
+    token = TOKEN
+    chat_id = CHAT_ID
+    url = f"{TELEGRAM_API_URL}{token}/sendPhoto"
 
-    bot.send_message(chat_id=chat_id, text=message)
+    send_message(message)
 
     converted_photos = download_and_convert_photos(photos)
     for photo in converted_photos:
         with open(photo, 'rb') as f:
-            bot.send_photo(chat_id=chat_id, photo=f)
+            files = {"photo": f}
+            params = {"chat_id": chat_id}
+            response = requests.post(url, params=params, files=files)
 
-        # os.remove(photo)
+            if response.status_code != 200:
+                print(f"Failed to send photo {photo}: {response.json()}")
+        os.remove(photo)
